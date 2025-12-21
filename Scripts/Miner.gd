1e8da05662ca313ea5306aad25cb5d378c0a142b
GeneDC <genecollis12@gmail.com>
2025-12-21 21:17:55 +1100

    removed formatter addon

--- Scripts/Miner.gd
+++ Scripts/Miner.gd
@@ -1,28 +1,25 @@
 extends Node3D
-
 class_name Miner
 
-@export var OreTypes: Array[MineGame.OreType] = []
+@export var ore_types: Array[MineGame.OreType] = []
 
-var TypeIndex: int = 0
+var type_index: int = 0
 
-var _Timer: Timer = null
-
+var _timer: Timer = null
 
 func _ready() -> void:
-	_Timer = Timer.new()
-	add_child(_Timer)
-	_Timer.wait_time = 0.5
-	_Timer.one_shot = false
-	_Timer.timeout.connect(SpawnOre)
-	_Timer.start()
-
+	_timer = Timer.new()
+	add_child(_timer)
+	_timer.wait_time = 0.5
+	_timer.one_shot = false
+	_timer.timeout.connect(SpawnOre)
+	_timer.start()
 
 func SpawnOre() -> void:
-	var oreType: MineGame.OreType = OreTypes[TypeIndex]
-	TypeIndex = (TypeIndex + 1) % OreTypes.size()
-	if (oreType == MineGame.OreType.None):
+	var ore_type: MineGame.OreType = ore_types[type_index]
+	type_index = (type_index + 1) % ore_types.size()
+	if (ore_type == MineGame.OreType.None):
 		return
 
-	var launchImpulse := Vector3(randf_range(-1, 1), 0, randf_range(-1, 1))
-	MineManager.ore_pool.InstantiateOre(oreType, global_position, launchImpulse)
+	var launch_impulse := Vector3(randf_range(-1, 1), 0, randf_range(-1, 1))
+	MineManager.ore_pool.InstantiateOre(ore_type, global_position, launch_impulse)
